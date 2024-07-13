65d2e3d1c1f5d04de00ec497ce11fc78ac936204
Mohammad.Arshad <arshadmckv@gmail.com>
2024-07-13 20:46:47 +0530

    Fist commit

---
+++ src/main/java/com/aquasec/purdue_devops_june_git/PurdueDevopsJuneGitApplication.java
@@ -0,0 +1,22 @@
+package com.aquasec.purdue_devops_june_git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class PurdueDevopsJuneGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PurdueDevopsJuneGitApplication.class, args);
+	}
+	
+	@GetMapping("/")
+    public String hello() {
+      return String.format("Hello this is a first GIT sample project");
+    }
+
+}
---
+++ src/test/java/com/aquasec/purdue_devops_june_git/PurdueDevopsJuneGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aquasec.purdue_devops_june_git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurdueDevopsJuneGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
